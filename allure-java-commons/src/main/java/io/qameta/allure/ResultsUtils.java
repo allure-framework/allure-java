eb6515b3e879cae6882f0f5a15c3e0212d45babc
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2017-02-23 20:42:19 +0300

    add an ability to override host and thread names via env

--- allure-java-commons/src/main/java/io/qameta/allure/ResultsUtils.java
+++ allure-java-commons/src/main/java/io/qameta/allure/ResultsUtils.java
@@ -2,10 +2,17 @@ package io.qameta.allure;
 
 import io.qameta.allure.model.Status;
 import io.qameta.allure.model.StatusDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 
 /**
@@ -13,7 +20,55 @@ import java.util.Optional;
  */
 public final class ResultsUtils {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResultsUtils.class);
+
+    public static final String ALLURE_HOST_NAME_SYSPROP = "allure.hostName";
+    public static final String ALLURE_HOST_NAME_ENV = "ALLURE_HOST_NAME";
+
+    public static final String ALLURE_THREAD_NAME_SYSPROP = "allure.threadName";
+    public static final String ALLURE_THREAD_NAME_ENV = "ALLURE_THREAD_NAME";
+
+    private static String CACHED_HOST = null;
+
     ResultsUtils() {
+        throw new IllegalStateException("Do not instance");
+    }
+
+    public static String getHostName() {
+        String fromProperty = System.getProperty(ALLURE_HOST_NAME_SYSPROP);
+        String fromEnv = System.getenv(ALLURE_HOST_NAME_ENV);
+        return Stream.of(fromProperty, fromEnv)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElseGet(ResultsUtils::getRealHostName);
+    }
+
+    private static String getRealHostName() {
+        if (Objects.isNull(CACHED_HOST)) {
+            try {
+                CACHED_HOST = InetAddress.getLocalHost().getHostName();
+            } catch (UnknownHostException e) { //NOSONAR
+                LOGGER.debug("Could not get host name {}", e);
+                CACHED_HOST = "default";
+            }
+        }
+        return CACHED_HOST;
+    }
+
+    public static String getTheadName() {
+        String fromProperty = System.getProperty(ALLURE_THREAD_NAME_SYSPROP);
+        String fromEnv = System.getenv(ALLURE_THREAD_NAME_ENV);
+        return Stream.of(fromProperty, fromEnv)
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElseGet(ResultsUtils::getRealTheadName);
+    }
+
+    private static String getRealTheadName() {
+        return String.format("%s.%s(%s)",
+                ManagementFactory.getRuntimeMXBean().getName(),
+                Thread.currentThread().getName(),
+                Thread.currentThread().getId());
     }
 
     public static Optional<Status> getStatus(Throwable throwable) {
