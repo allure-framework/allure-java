cd631154b68978f69c0106e35c22772b88779a4a
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2017-03-01 00:05:54 +0300

    add retry test

---
+++ allure-testng/src/test/java/io/qameta/allure/samples/RetryTest.java
@@ -0,0 +1,28 @@
+package io.qameta.allure.samples;
+
+import org.testng.ITestResult;
+import org.testng.annotations.Test;
+import org.testng.util.RetryAnalyzerCount;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+public class RetryTest {
+
+    @Test(retryAnalyzer = Retry.class)
+    public void testWithRetry() throws Exception {
+        throw new RuntimeException("Unexpected failure");
+    }
+
+    public static class Retry extends RetryAnalyzerCount {
+
+        @Override
+        public boolean retryMethod(ITestResult result) {
+            boolean willRetry = !result.isSuccess();
+            if (willRetry) {
+                result.setAttribute("retry", true);
+            }
+            return willRetry;
+        }
+    }
+}
