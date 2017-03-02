5560616fac49ee36b54b591f6948bc15bfbf6918
Dmitry Baev <baev.dm@gmail.com>
2017-03-02 20:34:38 +0300

    remove invalid javadocs

--- allure-java-commons/src/main/java/io/qameta/allure/Step.java
+++ allure-java-commons/src/main/java/io/qameta/allure/Step.java
@@ -6,21 +6,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * In order to define steps you need to annotate respective methods
- * with @Step annotation. When not specified step name equals to
- * annotated method name converted to human readable format. To
- * define explicit step name:
- * <pre>
- * &#064;Step("Open '{0}' page.")
- * public void openPageByAddress(String pageAddress) {
- *     ...
- * }
- * </pre>
- *
- * @author Dmitry Baev charlie@yandex-team.ru
- *         Date: 10.24.13
- */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
