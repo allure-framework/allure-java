fc2a1e7e1914f8be9572ce2f46bfbc27a51fda74
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2017-04-04 16:16:46 +0300

    first junit5 draft

---
+++ allure-junit5/src/test/java/io/qameta/allure/junit5/features/PassedTests.java
@@ -0,0 +1,22 @@
+package io.qameta.allure.junit5.features;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+public class PassedTests {
+
+    @Test
+    void first() {
+    }
+
+    @Test
+    void second() {
+    }
+
+    @Test
+    void third() {
+    }
+
+}
