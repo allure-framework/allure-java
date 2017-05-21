8dcf78f61b2aeab77217e12d4faf811ee663dbee
Sergey Korol <skorol@waverleysoftware.com>
2017-05-21 09:55:44 +0300

    - removed redundant step in a stream chain

--- allure-java-commons/src/main/java/io/qameta/allure/aspects/StepsAspects.java
+++ allure-java-commons/src/main/java/io/qameta/allure/aspects/StepsAspects.java
@@ -121,8 +121,7 @@ public class StepsAspects {
                                                     final Object... args) {
         return IntStream.range(0, args.length)
                         .filter(i -> parameterName.equals(signature.getParameterNames()[i]))
-                        .mapToObj(i -> args[i])
-                        .map(ob -> Objects.isNull(ob) ? Objects.toString(null) : ob)
+                        .mapToObj(i -> Objects.isNull(args[i]) ? Objects.toString(null) : args[i])
                         .findFirst()
                         .orElse(null);
     }
