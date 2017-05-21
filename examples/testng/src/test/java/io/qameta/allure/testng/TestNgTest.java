20d795988a523728268ba3dc9d12d9b48b5d189e
Dmitry Baev <baev.dm@gmail.com>
2017-05-21 19:44:46 +0300

    add examples

---
+++ examples/testng/src/test/java/io/qameta/allure/testng/TestNgTest.java
@@ -0,0 +1,13 @@
+package io.qameta.allure.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+public class TestNgTest {
+
+    @Test
+    public void testNgTest() throws Exception {
+    }
+}
