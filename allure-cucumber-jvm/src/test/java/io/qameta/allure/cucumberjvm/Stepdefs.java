a8320c4b7503e1b1381541f62664e25276a3df56
Viktor Sidochenko <viktor.sidochenko@gmail.com>
2017-06-01 21:21:00 +0700

    support data tables for cucumberjvm (fixes allure-framework/allure2#355, via #70)

--- allure-cucumber-jvm/src/test/java/io/qameta/allure/cucumberjvm/Stepdefs.java
+++ allure-cucumber-jvm/src/test/java/io/qameta/allure/cucumberjvm/Stepdefs.java
@@ -1,12 +1,12 @@
 package io.qameta.allure.cucumberjvm;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import io.qameta.allure.Attachment;
-
 import java.awt.AWTException;
 import java.awt.Rectangle;
 import java.awt.Robot;
@@ -15,7 +15,6 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-
 import org.junit.Assert;
 
 public class Stepdefs {
@@ -68,5 +67,9 @@ public class Stepdefs {
         } catch (AWTException | IOException e) {
             return null;
         }
+    }
+
+    @Given("^users are:$")
+    public void usersAre(DataTable table) throws Throwable {
     }
 }
