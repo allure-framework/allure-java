1021e3e7e085348969ceb0dc6c9ae3dcdbf644b9
egor.borisov <ehborisov@gmail.com>
2017-06-05 18:31:00 +0300

    change to after advice

--- allure-junit4-aspect/src/main/java/io/qameta/allure/junit4/AllureJunit4ListenerAspect.java
+++ allure-junit4-aspect/src/main/java/io/qameta/allure/junit4/AllureJunit4ListenerAspect.java
@@ -1,14 +1,10 @@
 package io.qameta.allure.junit4;
 
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.junit.runner.Description;
-import org.junit.runner.Result;
 import org.junit.runner.notification.RunNotifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Egor Borisov ehborisov@gmail.com
@@ -16,28 +12,16 @@ import org.slf4j.LoggerFactory;
 @Aspect
 public class AllureJunit4ListenerAspect {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AllureJunit4ListenerAspect.class);
     private final AllureJunit4 allureJunit4 = new AllureJunit4();
 
-    @Pointcut("execution(void org.junit.runners.ParentRunner.run(org.junit.runner.notification.RunNotifier))")
+    @Pointcut("execution(org.junit.runner.notification.RunNotifier.new())")
     public void run() {
         //empty pointcut body
     }
 
-    @Around("run()")
-    public void run(final ProceedingJoinPoint pjp) {
-        final Object[] args = pjp.getArgs();
-        final RunNotifier notifier = (RunNotifier) args[0];
-        notifier.removeListener(allureJunit4);
+    @After("run()")
+    public void run(JoinPoint point) {
+        final RunNotifier notifier = (RunNotifier) point.getThis();
         notifier.addListener(allureJunit4);
-        notifier.fireTestRunStarted(Description.createSuiteDescription("Tests"));
-
-        try {
-            pjp.proceed(args);
-        } catch (Throwable throwable) {
-            LOG.error("Exception on proceed: {}", throwable);
-        }
-
-        notifier.fireTestRunFinished(new Result());
     }
 }
