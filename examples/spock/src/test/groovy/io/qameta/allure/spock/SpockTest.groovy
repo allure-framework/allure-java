135ff2f3e3c9ca531446a69c02c98fdc9618bf89
Yuri Kudryavtsev <kyurik@yandex.ru>
2017-06-26 16:03:44 +0300

    add spock example (via #85)

---
+++ examples/spock/src/test/groovy/io/qameta/allure/spock/SpockTest.groovy
@@ -0,0 +1,18 @@
+package io.qameta.allure.spock
+
+import spock.lang.Specification
+
+/**
+ * Created on 16.06.2017
+ *
+ * @author Yuri Kudryavtsev
+ *         skype: yuri.kudryavtsev.indeed
+ *         email: yuri.kudryavtsev@indeed-id.com
+ */
+
+class SpockTest extends Specification {
+    def "Example of Spock Test"() {
+        expect:
+        true
+    }
+}
