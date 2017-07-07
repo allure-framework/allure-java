010eb61c9904e57c696ee7661cca17cbf75e06af
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2017-07-07 16:48:01 +0300

    remove wildcard imports

--- allure-junit4/src/main/java/io/qameta/allure/junit4/Tag.java
+++ allure-junit4/src/main/java/io/qameta/allure/junit4/Tag.java
@@ -1,11 +1,16 @@
 package io.qameta.allure.junit4;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @author jkttt on 05.07.17.
  */
-@Target({ ElementType.TYPE, ElementType.METHOD })
+@Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Repeatable(Tags.class)
