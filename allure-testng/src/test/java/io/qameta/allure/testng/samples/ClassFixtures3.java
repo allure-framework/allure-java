95a629f3084ef0db3c996556993fee2b9741b616
Dmitry Baev <baev.dm@gmail.com>
2017-07-09 21:59:49 +0300

    fix class fixtures (fixes #99, via #100)

---
+++ allure-testng/src/test/java/io/qameta/allure/testng/samples/ClassFixtures3.java
@@ -0,0 +1,23 @@
+package io.qameta.allure.testng.samples;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+public class ClassFixtures3 {
+
+    @BeforeClass
+    public void beforeClass3() throws Exception {
+    }
+
+    @Test
+    public void classFixtures3() throws Exception {
+    }
+
+    @AfterClass
+    public void afterClass3() throws Exception {
+    }
+}
