ed0a483a19bf341a985339d33ad27c7e14f964d6
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2017-07-10 16:47:11 +0300

    improve history id calculation (fixes #102, via #103)

---
+++ allure-testng/src/test/java/io/qameta/allure/testng/samples/JustTest.java
@@ -0,0 +1,14 @@
+package io.qameta.allure.testng.samples;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+public class JustTest {
+
+    @Test
+    public void shouldTest() throws Exception {
+    }
+
+}
