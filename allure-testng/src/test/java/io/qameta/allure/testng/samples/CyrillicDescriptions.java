b19aaa68df0e133ace0622cd93d1effca6bf1906
Dmitry Baev <baev.dm@gmail.com>
2017-07-16 13:58:50 +0300

    add test for encoding

---
+++ allure-testng/src/test/java/io/qameta/allure/testng/samples/CyrillicDescriptions.java
@@ -0,0 +1,13 @@
+package io.qameta.allure.testng.samples;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+public class CyrillicDescriptions {
+
+    @Test(description = "Тест с описанием на русском языке")
+    public void testWithCyrillicDescription() throws Exception {
+    }
+}
