856159e0607d88e209131f12498666c83c8e8311
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2017-08-07 16:04:50 +0300

    junit4 - сombining repeatable annotations on method and on class (via #121)

--- allure-junit4/src/test/java/io/qameta/allure/junit4/samples/TaggedTests.java
+++ allure-junit4/src/test/java/io/qameta/allure/junit4/samples/TaggedTests.java
@@ -7,11 +7,13 @@ import org.junit.Test;
 /**
  * @author jkttt on 05.07.17.
  */
+@Tags({@Tag(TaggedTests.CLASS_TAG1), @Tag(TaggedTests.CLASS_TAG2)})
 public class TaggedTests {
 
     public static final String METHOD_TAG2 = "method_tag1";
     public static final String METHOD_TAG1 = "method_tag2";
-    public static final String CLASS_TAG = "class_tag";
+    public static final String CLASS_TAG1 = "class_tag1";
+    public static final String CLASS_TAG2 = "class_tag2";
 
     @Test
     @Tags({@Tag(METHOD_TAG1),
