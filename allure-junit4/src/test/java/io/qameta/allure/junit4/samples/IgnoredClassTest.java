ec171cf89f8efbfd213a1f602402497559fef73c
Nikolai Gladkov <gladnik@ya.ru>
2017-08-07 16:01:16 +0300

    junit4 - fix npe with ignore annotation on classes  (via #119)

---
+++ allure-junit4/src/test/java/io/qameta/allure/junit4/samples/IgnoredClassTest.java
@@ -0,0 +1,16 @@
+package io.qameta.allure.junit4.samples;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author gladnik (Nikolai Gladkov)
+ */
+@Ignore
+public class IgnoredClassTest {
+
+    @Test
+    public void ignoredAsPartOfTheIgnoredClassTest() {
+    }
+
+}
