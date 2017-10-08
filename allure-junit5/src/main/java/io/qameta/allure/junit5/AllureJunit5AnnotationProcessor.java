4ef4de0dacf0f41d7cf52b6aaa674526b2e25eeb
Artem Eroshenko <eroshenkoam@qameta.io>
2017-10-08 23:19:05 +0300

    remove unused imports

--- allure-junit5/src/main/java/io/qameta/allure/junit5/AllureJunit5AnnotationProcessor.java
+++ allure-junit5/src/main/java/io/qameta/allure/junit5/AllureJunit5AnnotationProcessor.java
@@ -22,7 +22,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
