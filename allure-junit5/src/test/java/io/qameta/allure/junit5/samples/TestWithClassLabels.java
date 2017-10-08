c9435e8f1870ac23a44894237e550a8b8862d64a
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2017-10-09 00:35:38 +0300

    junit5 annotations support (via #154)

---
+++ allure-junit5/src/test/java/io/qameta/allure/junit5/samples/TestWithClassLabels.java
@@ -0,0 +1,31 @@
+package io.qameta.allure.junit5.samples;
+
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Stories;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+@Epic("epic1")
+@Epic("epic2")
+@Epic("epic3")
+@Feature("feature1")
+@Feature("feature2")
+@Feature("feature3")
+@Story("story1")
+@Stories({
+        @Story("story2"),
+        @Story("story3")
+})
+@Owner("some-owner")
+public class TestWithClassLabels {
+
+    @Test
+    public void someTest() throws Exception {
+    }
+
+}
---
+++ allure-junit5/src/test/java/io/qameta/allure/junit5/samples/TestWithClassLinks.java
@@ -0,0 +1,32 @@
+package io.qameta.allure.junit5.samples;
+
+import io.qameta.allure.Issue;
+import io.qameta.allure.Issues;
+import io.qameta.allure.Link;
+import io.qameta.allure.Links;
+import io.qameta.allure.TmsLink;
+import io.qameta.allure.TmsLinks;
+import org.junit.jupiter.api.Test;
+
+@Link(name = "LINK-1")
+@Links({
+        @Link(name = "LINK-2"),
+        @Link(name = "LINK-3")
+})
+@TmsLink("TMS-1")
+@TmsLinks({
+        @TmsLink("TMS-2"),
+        @TmsLink("TMS-3")
+})
+@Issue("ISSUE-1")
+@Issues({
+        @Issue("ISSUE-2"),
+        @Issue("ISSUE-3")
+})
+public class TestWithClassLinks {
+
+    @Test
+    public void someTest() throws Exception {
+    }
+
+}
