1beecc3a0f3cb349088b3ae4ddb0310a57e961b2
Mayer Dmitriy Yakovlevich <majer-dy@fxclub.org>
2017-10-17 19:54:28 +0200

    Enable all @good tests by default

--- allure-cucumber2-jvm/src/test/java/io/qameta/allure/cucumber2jvm/CucumberTest.java
+++ allure-cucumber2-jvm/src/test/java/io/qameta/allure/cucumber2jvm/CucumberTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/features/test-examples.feature"},
+        features = {"src/test/resources/features/"},
         plugin = {"io.qameta.allure.cucumber2jvm.AllureCucumber2Jvm"},
         tags = {"@good"})
 public class CucumberTest {
