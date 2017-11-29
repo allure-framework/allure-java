a74eb2c617ef2b5f0e57d4e344d1a7742a2a3d08
Artem Eroshenko <eroshenkoam@qameta.io>
2017-11-29 11:29:46 +0100

    First draft of selenide integration

---
+++ allure-selenide/src/main/java/io/qameta/allure/selenide/AllureSelenide.java
@@ -0,0 +1,66 @@
+package io.qameta.allure.selenide;
+
+import com.codeborne.selenide.impl.ScreenShotLaboratory;
+import com.codeborne.selenide.logevents.LogEvent;
+import com.codeborne.selenide.logevents.LogEventListener;
+import io.qameta.allure.Allure;
+import io.qameta.allure.AllureLifecycle;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StatusDetails;
+import io.qameta.allure.model.StepResult;
+import io.qameta.allure.util.ResultsUtils;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @author Artem Eroshenko.
+ */
+public class AllureSelenide implements LogEventListener {
+
+    private final AllureLifecycle lifecycle;
+
+    public AllureSelenide() {
+        this(Allure.getLifecycle());
+    }
+
+    public AllureSelenide(AllureLifecycle lifecycle) {
+        this.lifecycle = lifecycle;
+    }
+
+    @Override
+    public void onEvent(LogEvent event) {
+        lifecycle.getCurrentTestCase().ifPresent(uuid -> {
+            String stepUUID = UUID.randomUUID().toString();
+            lifecycle.startStep(stepUUID, new StepResult()
+                    .withName(event.toString())
+                    .withStatus(Status.PASSED));
+
+            if (LogEvent.EventStatus.FAIL.equals(event.getStatus())) {
+                byte[] screenshotBytes = getScreenshot();
+                lifecycle.addAttachment("Screenshot", "image/png", "png", screenshotBytes);
+                lifecycle.updateStep(stepResult -> {
+                    StatusDetails details = ResultsUtils.getStatusDetails(event.getError()).orElse(new StatusDetails());
+                    stepResult.setStatus(Status.FAILED);
+                    stepResult.setStatusDetails(details);
+                });
+            }
+            lifecycle.stopStep(stepUUID);
+        });
+    }
+
+
+    private byte[] getScreenshot() {
+        ScreenShotLaboratory screenshots = new ScreenShotLaboratory();
+        File file = screenshots.takeScreenShotAsFile();
+        try {
+            return FileUtils.readFileToByteArray(file);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            FileUtils.deleteQuietly(file);
+        }
+    }
+}
