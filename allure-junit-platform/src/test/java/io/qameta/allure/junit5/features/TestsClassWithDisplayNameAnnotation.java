f444a8d2009bc8801e1679fc50ec23dcfb4b8499
Afrikanov Andrey <afrikanov.andrey@gmail.com>
2017-12-03 22:39:50 +0300

    [Allure_2 + JUnit_5] Added applying:   - @Description for test methods   - @DisplayName for test classes

---
+++ allure-junit-platform/src/test/java/io/qameta/allure/junit5/features/TestsClassWithDisplayNameAnnotation.java
@@ -0,0 +1,15 @@
+package io.qameta.allure.junit5.features;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author a.afrikanov (Andrey Afrikanov).
+ */
+@DisplayName("Display name of test class")
+public class TestsClassWithDisplayNameAnnotation {
+
+    @Test
+    void test() {
+    }
+}
