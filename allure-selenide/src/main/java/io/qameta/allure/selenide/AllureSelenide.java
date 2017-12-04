0e325d6492a4623f2be229027f90ee4342bdd660
Artem Eroshenko <eroshenkoam@qameta.io>
2017-12-04 19:15:17 +0300

    selenide integration (via #169)

---
+++ allure-selenide/src/main/java/io/qameta/allure/selenide/AllureSelenide.java
@@ -0,0 +1,66 @@
+package io.qameta.allure.selenide;
+
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.logevents.LogEvent;
+import com.codeborne.selenide.logevents.LogEventListener;
+import io.qameta.allure.Allure;
+import io.qameta.allure.AllureLifecycle;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StatusDetails;
+import io.qameta.allure.model.StepResult;
+import io.qameta.allure.util.ResultsUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
+
+/**
+ * @author Artem Eroshenko.
+ */
+public class AllureSelenide implements LogEventListener {
+
+    private final AllureLifecycle lifecycle;
+
+    public AllureSelenide() {
+        this(Allure.getLifecycle());
+    }
+
+    public AllureSelenide(final AllureLifecycle lifecycle) {
+        this.lifecycle = lifecycle;
+    }
+
+    @Override
+    public void onEvent(final LogEvent event) {
+        lifecycle.getCurrentTestCase().ifPresent(uuid -> {
+            final String stepUUID = UUID.randomUUID().toString();
+            lifecycle.startStep(stepUUID, new StepResult()
+                    .withName(event.toString())
+                    .withStatus(Status.PASSED));
+
+            lifecycle.updateStep(stepResult -> stepResult.setStart(stepResult.getStart() - event.getDuration()));
+
+            if (LogEvent.EventStatus.FAIL.equals(event.getStatus())) {
+                lifecycle.addAttachment("Screenshot", "image/png", "png", getScreenshotBytes());
+                lifecycle.addAttachment("Page source", "text/html", "html", getPageSourceBytes());
+                lifecycle.updateStep(stepResult -> {
+                    final StatusDetails details = ResultsUtils.getStatusDetails(event.getError())
+                            .orElse(new StatusDetails());
+                    stepResult.setStatus(Status.FAILED);
+                    stepResult.setStatusDetails(details);
+                });
+            }
+            lifecycle.stopStep(stepUUID);
+        });
+    }
+
+
+    private static byte[] getScreenshotBytes() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
+    private static byte[] getPageSourceBytes() {
+        return WebDriverRunner.getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+    }
+
+}
