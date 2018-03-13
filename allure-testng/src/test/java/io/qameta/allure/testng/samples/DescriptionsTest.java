432996c299c78f71df0a4f64bf3795988061e017
sergey.potanin <sspotanin@gmail.com>
2018-03-13 17:42:48 +0300

    add javadoc description to before fixture htmlDescription

--- allure-testng/src/test/java/io/qameta/allure/testng/samples/DescriptionsTest.java
+++ allure-testng/src/test/java/io/qameta/allure/testng/samples/DescriptionsTest.java
@@ -2,12 +2,32 @@ package io.qameta.allure.testng.samples;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
  * @author Egor Borisov ehborisov@gmail.com
  */
 public class DescriptionsTest {
+
+    /**
+     * Before class description
+     */
+    @BeforeClass
+    @Description(useJavaDoc = true)
+    public void setUpClass() {
+
+    }
+
+    /**
+     * Before method description
+     */
+    @BeforeMethod
+    @Description(useJavaDoc = true)
+    public void setUpMethod() {
+
+    }
 
     /**
      * Sample test description
