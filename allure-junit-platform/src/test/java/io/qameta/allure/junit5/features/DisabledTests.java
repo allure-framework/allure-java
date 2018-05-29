93ba0ba48be6545e570fdc102317b5b66b31fd5e
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2018-05-29 15:55:42 +0300

    junit5 - handle disabled test (via #222)

---
+++ allure-junit-platform/src/test/java/io/qameta/allure/junit5/features/DisabledTests.java
@@ -0,0 +1,13 @@
+package io.qameta.allure.junit5.features;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class DisabledTests {
+
+    @Test
+    @Disabled
+    void disabledByAnnotation() {
+
+    }
+}
