d5dc1ae879e5077d4dbd344d1d255743f8a22179
Ervīns Patmalnieks <ervins.patmalnieks@gmail.com>
2018-11-01 14:32:45 +0300

    add host and thread labels for junit platform tests (via #260)

---
+++ allure-junit-platform/src/test/java/io/qameta/allure/junitplatform/features/OneTest.java
@@ -0,0 +1,13 @@
+package io.qameta.allure.junitplatform.features;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author jkttt on 05.07.17.
+ */
+public class OneTest {
+
+    @Test
+    void single() {
+    }
+}
