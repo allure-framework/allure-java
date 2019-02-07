dc86ac1c716667117e444b64a1bc2f98384f1b5f
choojoykin <pavlof.vladimir@gmail.com>
2019-02-07 16:35:27 +0300

    add spring-web integration (via #318)

---
+++ allure-spring-web/build.gradle.kts
@@ -0,0 +1,37 @@
+description = "Allure Spring Web Integration"
+
+val agent by configurations.creating
+
+val springWebVersion = "5.0.4.RELEASE"
+
+dependencies {
+    agent("org.aspectj:aspectjweaver")
+
+    compile(project(":allure-attachments"))
+    compile("org.springframework:spring-web:$springWebVersion")
+    testCompile("com.github.tomakehurst:wiremock")
+    testCompile("org.jboss.resteasy:resteasy-client")
+    testCompile("org.assertj:assertj-core")
+    testCompile("org.junit.jupiter:junit-jupiter-api")
+    testCompile("org.junit.jupiter:junit-jupiter-params")
+    testCompile("org.mockito:mockito-core")
+    testCompile("org.slf4j:slf4j-simple")
+    testCompile(project(":allure-java-commons-test"))
+    testCompile(project(":allure-junit-platform"))
+    testRuntime("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.named<Jar>("jar") {
+    manifest {
+        attributes(mapOf(
+                "Automatic-Module-Name" to "io.qameta.allure.springweb"
+        ))
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+    doFirst {
+        jvmArgs("-javaagent:${agent.singleFile}")
+    }
+}
---
+++ allure-spring-web/src/main/java/io/qameta/allure/springweb/AllureRestTemplate.java
@@ -0,0 +1,75 @@
+package io.qameta.allure.springweb;
+
+import io.qameta.allure.attachment.AttachmentData;
+import io.qameta.allure.attachment.AttachmentProcessor;
+import io.qameta.allure.attachment.DefaultAttachmentProcessor;
+import io.qameta.allure.attachment.FreemarkerAttachmentRenderer;
+import io.qameta.allure.attachment.http.HttpRequestAttachment;
+import io.qameta.allure.attachment.http.HttpResponseAttachment;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.lang.NonNull;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Allure interceptor for spring rest template.
+ */
+public class AllureRestTemplate implements ClientHttpRequestInterceptor {
+
+    private String requestTemplatePath = "http-request.ftl";
+    private String responseTemplatePath = "http-response.ftl";
+
+    public AllureRestTemplate setRequestTemplate(final String templatePath) {
+        this.requestTemplatePath = templatePath;
+        return this;
+    }
+
+    public AllureRestTemplate setResponseTemplate(final String templatePath) {
+        this.responseTemplatePath = templatePath;
+        return this;
+    }
+
+    @SuppressWarnings("NullableProblems")
+    @Override
+    public ClientHttpResponse intercept(@NonNull final HttpRequest request, final byte[] body,
+                                        @NonNull final ClientHttpRequestExecution execution) throws IOException {
+        final AttachmentProcessor<AttachmentData> processor = new DefaultAttachmentProcessor();
+
+        final HttpRequestAttachment.Builder requestAttachmentBuilder = HttpRequestAttachment.Builder
+                .create("Request", request.getURI().toString())
+                .setMethod(request.getMethodValue())
+                .setHeaders(toMapConverter(request.getHeaders()));
+        if (body.length != 0) {
+            requestAttachmentBuilder.setBody(new String(body, StandardCharsets.UTF_8));
+        }
+
+        final HttpRequestAttachment requestAttachment = requestAttachmentBuilder.build();
+        processor.addAttachment(requestAttachment, new FreemarkerAttachmentRenderer(requestTemplatePath));
+
+        final ClientHttpResponse clientHttpResponse = execution.execute(request, body);
+
+        final HttpResponseAttachment responseAttachment = HttpResponseAttachment.Builder
+                .create("Response")
+                .setResponseCode(clientHttpResponse.getRawStatusCode())
+                .setHeaders(toMapConverter(clientHttpResponse.getHeaders()))
+                .setBody(StreamUtils.copyToString(clientHttpResponse.getBody(), StandardCharsets.UTF_8))
+                .build();
+        processor.addAttachment(responseAttachment, new FreemarkerAttachmentRenderer(responseTemplatePath));
+
+        return clientHttpResponse;
+    }
+
+    private static Map<String, String> toMapConverter(final Map<String, List<String>> items) {
+        final Map<String, String> result = new HashMap<>();
+        items.forEach((key, value) -> result.put(key, String.join("; ", value)));
+        return result;
+    }
+}
