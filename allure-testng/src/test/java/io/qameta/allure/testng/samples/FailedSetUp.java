dbb6ac0171f4084003ec0648d31c2a15b41ee1bf
Dmitry Baev <baev.dm@gmail.com>
2019-02-12 21:22:38 +0300

    add test for an issue (fixes #304, via #325)

---
+++ allure-testng/src/test/java/io/qameta/allure/testng/samples/FailedSetUp.java
@@ -0,0 +1,43 @@
+/*
+ *  Copyright 2019 Qameta Software OÜ
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.qameta.allure.testng.samples;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+public class FailedSetUp {
+
+    @BeforeMethod
+    public void setUp() {
+        throw new RuntimeException("Hey");
+    }
+
+    @Test
+    public void skippedTest() {
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void afterAlways() {
+        step("first");
+        step("second");
+    }
+}
