e90763ba0255c6a20293fb257bea36e3505bf2b9
jsa34 <31512041+jsa34@users.noreply.github.com>
2019-02-12 12:59:48 +0000

    Bump cucumberjvm

--- allure-cucumber4-jvm/build.gradle.kts
+++ allure-cucumber4-jvm/build.gradle.kts
@@ -2,7 +2,7 @@ description = "Allure CucumberJVM 4.0"
 
 val agent by configurations.creating
 
-val cucumberVersion = "4.2.2"
+val cucumberVersion = "4.2.3"
 
 dependencies {
     agent("org.aspectj:aspectjweaver")
