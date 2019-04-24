7bab0ff4ac4dfc46914fe4bd2be2523ecb417dd8
Dmitry Baev <baev.dm@gmail.com>
2019-04-24 16:00:27 +0300

    add support for test instance params

---
+++ allure-testng/src/test/java/io/qameta/allure/testng/samples/FactoryOnConstructorTests.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright 2019 Qameta Software OÜ
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.qameta.allure.testng.samples;
+
+import io.qameta.allure.testng.TestInstanceParameter;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+public class FactoryOnConstructorTests {
+
+    @TestInstanceParameter
+    private int number;
+
+    @TestInstanceParameter("Name")
+    private String v;
+
+    private Long other;
+
+    @Factory(dataProvider = "dataProvider")
+    public FactoryOnConstructorTests(final int number, final String v, final Long other) {
+        this.number = number;
+        this.v = v;
+        this.other = other;
+    }
+
+    @DataProvider
+    public static Object[][] dataProvider() {
+        return new Object[][]{new Object[]{1, "first", 22L}, new Object[]{2, "second", 23L}};
+    }
+
+    @Test
+    public void factoryTest() {
+    }
+}
