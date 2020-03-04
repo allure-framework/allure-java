4853d2929feaff9fd06fb1d8be4e5f4c9eb84441
alexey-shnaydmiller <alexeylyle@gmail.com>
2020-03-04 16:57:44 +0700

    Update build.gradle.kts
    commons lang3 collections4

--- allure-spock/build.gradle.kts
+++ allure-spock/build.gradle.kts
@@ -5,11 +5,15 @@ apply(plugin = "groovy")
 val agent: Configuration by configurations.creating
 
 val spockFrameworkVersion = "1.2-groovy-2.5"
+val apacheCommonsCollection4 = "4.4"
+val apacheCommonsLang3 = "3.9"
 
 dependencies {
     agent("org.aspectj:aspectjweaver")
     api(project(":allure-java-commons"))
     implementation("org.spockframework:spock-core:$spockFrameworkVersion")
+    implementation("org.apache.commons:commons-collections4:$apacheCommonsCollection4")
+    implementation("org.apache.commons:commons-lang3:$apacheCommonsLang3")
     testImplementation("org.assertj:assertj-core")
     testImplementation("org.codehaus.groovy:groovy-all")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
