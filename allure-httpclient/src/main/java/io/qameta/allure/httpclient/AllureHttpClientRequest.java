6e5013e79d796184117955020118d75f43dff9d8
Bartek Florczak <bflorczak@outlook.com>
2020-06-19 16:16:23 +0200

    Split branch

--- allure-httpclient/src/main/java/io/qameta/allure/httpclient/AllureHttpClientRequest.java
+++ allure-httpclient/src/main/java/io/qameta/allure/httpclient/AllureHttpClientRequest.java
@@ -59,16 +59,17 @@ public class AllureHttpClientRequest implements HttpRequestInterceptor {
         Stream.of(request.getAllHeaders())
               .forEach(header -> builder.setHeader(header.getName(), header.getValue()));
 
-        if (request instanceof HttpEntityEnclosingRequest && ((HttpEntityEnclosingRequest) request).getEntity() != null) {
+        if (request instanceof HttpEntityEnclosingRequest) {
             final HttpEntity entity = ((HttpEntityEnclosingRequest) request).getEntity();
 
-            final ByteArrayOutputStream os = new ByteArrayOutputStream();
-            entity.writeTo(os);
+            if (entity != null) {
+                final ByteArrayOutputStream os = new ByteArrayOutputStream();
+                entity.writeTo(os);
 
-            final String body = new String(os.toByteArray(), StandardCharsets.UTF_8);
-            builder.setBody(body);
+                final String body = new String(os.toByteArray(), StandardCharsets.UTF_8);
+                builder.setBody(body);
+            }
         }
-
         final HttpRequestAttachment requestAttachment = builder.build();
         processor.addAttachment(requestAttachment, renderer);
     }
