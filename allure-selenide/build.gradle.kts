b7e516a0a1a9a178de97e8f943c50b26fa38bbfa
10koteyka01 <tihonenkovk@mail.ru>
2020-06-30 16:25:24 +0700

    bump selenide version (via #451)

--- allure-selenide/build.gradle.kts
+++ allure-selenide/build.gradle.kts
@@ -2,7 +2,7 @@ description = "Allure Selenide Integration"
 
 val agent: Configuration by configurations.creating
 
-val selenideVersion = "5.11.0"
+val selenideVersion = "5.12.2"
 
 dependencies {
     agent("org.aspectj:aspectjweaver")
