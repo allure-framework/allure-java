6405784bca9bd527da3a565ca3b3971a3e5fdc2d
Nikolay Laptev <neparij@mail.ru>
2022-07-04 11:47:18 +0300

    Review fixes

--- allure-testng/src/main/java/io/qameta/allure/testng/Tag.java
+++ allure-testng/src/main/java/io/qameta/allure/testng/Tag.java
@@ -17,7 +17,12 @@ package io.qameta.allure.testng;
 
 import io.qameta.allure.LabelAnnotation;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import static io.qameta.allure.util.ResultsUtils.TAG_LABEL_NAME;
 
--- allure-testng/src/main/java/io/qameta/allure/testng/Tags.java
+++ allure-testng/src/main/java/io/qameta/allure/testng/Tags.java
@@ -15,7 +15,11 @@
  */
 package io.qameta.allure.testng;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * @author neparij (Nikolay Laptev).
