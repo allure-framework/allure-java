8a8dbaafb2e53985cfe218461afcd79fd53f5d59
Dmitry Baev <baev.dm@gmail.com>
2022-10-26 16:48:47 +0700

    spock 2 (fixes #239, fixes #510, via #838)

---
+++ allure-spock2/build.gradle.kts
@@ -0,0 +1,60 @@
+description = "Allure Spock 2 Framework Integration"
+
+plugins {
+    groovy
+}
+
+val spockFrameworkVersion = "2.3-groovy-3.0"
+val groovyVersion = "3.0.13"
+
+dependencies {
+    api(project(":allure-java-commons"))
+    implementation("org.spockframework:spock-core:$spockFrameworkVersion")
+    compileOnly("org.aspectj:aspectjrt")
+    implementation(project(":allure-test-filter"))
+    testAnnotationProcessor("org.slf4j:slf4j-simple")
+    testAnnotationProcessor(project(":allure-descriptions-javadoc"))
+    testImplementation("io.github.glytching:junit-extensions")
+    testImplementation("org.assertj:assertj-core")
+    testImplementation("org.codehaus.groovy:groovy-all:${groovyVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.mockito:mockito-core")
+    testImplementation("org.slf4j:slf4j-simple")
+    testImplementation(project(":allure-assertj"))
+    testImplementation(project(":allure-java-commons-test"))
+    testImplementation(project(":allure-junit-platform"))
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks.jar {
+    manifest {
+        attributes(mapOf(
+                "Automatic-Module-Name" to "io.qameta.allure.spock2"
+        ))
+    }
+
+    from("src/main/services") {
+        into("META-INF/services")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+    exclude("**/samples/*")
+}
+
+val spiOffJar: Jar by tasks.creating(Jar::class) {
+    from(sourceSets.getByName("main").output)
+    archiveClassifier.set("spi-off")
+}
+
+publishing {
+    publications {
+        named<MavenPublication>("maven") {
+            artifact(spiOffJar)
+        }
+    }
+}
+
---
+++ allure-spock2/src/test/groovy/io/qameta/allure/spock2/samples/StepsAndBlocks.groovy
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2019 Qameta Software OÜ
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package io.qameta.allure.spock2.samples
+
+import io.qameta.allure.Step
+import spock.lang.Specification
+
+import static io.qameta.allure.Allure.step
+
+/**
+ * @author charlie (Dmitry Baev).
+ */
+
+class StepsAndBlocks extends Specification {
+
+    def "test with steps and blocks"() {
+        given: "asd"
+        step "step1"
+        step "step2"
+        "step some"()
+        when:
+        step "step3"
+        step "step4"
+        then:
+        step "step5"
+        step "step6"
+    }
+
+    @Step
+    def "step some"() {
+        println "yaaa"
+    }
+}
