89462e5125609eef469aa1d47f288a7e470865b8
Dmitry Baev <baev@users.noreply.github.com>
2024-03-04 18:21:06 +0000

    backport change to cucumber 5

--- allure-cucumber5-jvm/build.gradle.kts
+++ allure-cucumber5-jvm/build.gradle.kts
@@ -15,7 +15,9 @@ dependencies {
     testImplementation("org.assertj:assertj-core")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("org.slf4j:slf4j-simple")
+    testImplementation(project(":allure-assertj"))
     testImplementation(project(":allure-java-commons-test"))
+    testImplementation(project(":allure-junit-platform"))
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 }
 
