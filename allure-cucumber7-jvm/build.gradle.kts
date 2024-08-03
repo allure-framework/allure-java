d7b4969d1390781abe5f4d2a8c6e3eabeb5b0a66
DineshKumarRA <dinesh.asok@gmail.com>
2024-08-03 15:51:54 +0100

    Updated cucumber 7 versions

--- allure-cucumber7-jvm/build.gradle.kts
+++ allure-cucumber7-jvm/build.gradle.kts
@@ -1,7 +1,7 @@
 description = "Allure CucumberJVM 7.0"
 
-val cucumberVersion = "7.13.0"
-val cucumberGherkinVersion = "26.2.0"
+val cucumberVersion = "7.18.1"
+val cucumberGherkinVersion = "28.0.0"
 
 dependencies {
     api(project(":allure-java-commons"))
